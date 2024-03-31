47e7f18efd6d976bbe6fde0a0ba1f0d85d2df328
Andres-Hincapie-HRZ <164922026+Andres-Hincapie-HRZ@users.noreply.github.com>
2024-03-31 14:48:10 -0500

    Update Ejercicio 12 Tarea.py

--- Clase #2 Ejercicios/Ejercicio 12 Tarea.py
+++ Clase #2 Ejercicios/Ejercicio 12 Tarea.py
@@ -1,3 +1,10 @@
+'''"Escribe un programa que te ayude a saber si un número es positivo, negativo o cero. 
+Primero, pídele al usuario que te dé un número. Después, verifica si el número que te dio es mayor que 0, menor que 0 o igual a 0.
+Si es mayor que 0, dile al usuario que el número es positivo. Si es menor que 0, dile que es negativo. 
+Y si es igual a 0, dile que es cero. Finalmente, muéstrale al usuario el resultado.
+(Sin usar if, else o elif) 
+'''
+
 num = int(input("Ingresa un número: "))
 resultado = "positivo" * (num > 0) + "negativo" * (num < 0) + "cero" * (num == 0)
 print("El número es", resultado)
